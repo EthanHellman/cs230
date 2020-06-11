7cefd8bd53a28cbacbd36ece5b0fe975402716e8
Ethan Hellman <emanhellman@gmail.com>
2020-06-11 11:28:16 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy import array
 from keras.models import Sequential
 from keras.layers import Dense
@@ -11,11 +12,11 @@ from keras.optimizers import Adam
 
 
 
-def model(input_shape):
+def create_model(input_shape):
 
 	X_input = Input(shape = input_shape)
 
-	X = Conv1D(filters = 196, kernel_size = 15, strides = 4)(X_Input)
+	X = Conv1D(filters = 196, kernel_size = 15, strides = 4)(X_input)
 
 	X = GRU(units = 128, return_sequences = True)(X)
 
@@ -23,17 +24,17 @@ def model(input_shape):
 
 	X = Activation('relu')(X)
 
-	X = Dropout(rate = .8)(X)
+	X = Dropout(rate = .2)(X)
 
-	X = GRU(units = 128, kernel_size = 15, strides = 4)(X)
+	X = GRU(units = 128, return_sequences = True)(X)
 
-	X = Dropout(rate = .8)(X)
+	X = Dropout(rate = .2)(X)
 
 	X = BatchNormalization()(X)
 
-	X = Dropout(rate = .8)
+	X = Dropout(rate = .2)(X)
 
-	X = TimeDistributed(Dense(1, activation = 'sigmoid'))(X)
+	X = TimeDistributed(Dense(15, activation = 'sigmoid'))(X)
 
 	model = Model(inputs = X_input, outputs = X)
 
@@ -41,14 +42,19 @@ def model(input_shape):
 
 
 def main():
-	X_train = np.load("X_Train")
-	Y_train = np.load("Y_Train")
+	X_train = np.load("X_Train.npy")
+	Y_train = np.load("Y_Train.npy")
 
-	X_Dev = np.load("X_Dev")
-	Y_Dev = np.load("Y_Dev")
+	X_Dev = np.load("X_Dev.npy")
+	Y_Dev = np.load("Y_Dev.npy")
+
+	Y_train = np.reshape(Y_train, (np.shape(Y_train)[0], 1, 15))
+
+	Y_Dev = np.reshape(Y_Dev, (np.shape(Y_Dev)[0], 1, 15))
 
 
-	model = model(input_shape = (15, 1078))
+	model = create_model(input_shape = (15, 1078))
+
 	model.summary()
 
 	opt = Adam(lr = 0.0001, beta_1 = .9, beta_2 = .999, decay = .01)
@@ -58,7 +64,7 @@ def main():
 
 	loss, accuracy = model.evaluate(X_Dev, Y_Dev)
 
-	print("Dev set accuracy:" accuracy)
+	print("Dev set accuracy:" + str(accuracy))
 
 	model.save_weights("weights.h5")
 
